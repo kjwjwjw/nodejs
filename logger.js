16fb4df89f3291982b735faecc5723f4ffd0ab1c
kjwjwjw <(tbs03112@naver.com)>
2024-03-23 15:13:35 +0900

    commit

---
+++ logger.js
@@ -0,0 +1,9 @@
+function showLogMessage(msg) {
+
+    console.log(`-----------------------`)
+    console.log(`로그메시지는 : ${msg}`)
+    console.log(`-----------------------`)
+}
+
+
+module.exports.showLogMessage = showLogMessage
---
+++ test.js
@@ -0,0 +1,11 @@
+//function sayHello(name) {
+    // console.log('Hello' +name)
+// }
+
+// sayHello('John')
+
+// if (v > 5)
+
+const logger = require('./logger.js')
+
+logger.showLogMessage('모듈에 대한 테스트 중입니다.')
